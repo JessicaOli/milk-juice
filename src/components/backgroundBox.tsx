e2885c370b28da6252ccacd6b4e3f0a83ee5bc46
JessicaOli <jequinha_olivenca@hotmail.com>
2023-10-03 22:15:26 +0100

    Initial UI setup

---
+++ src/components/backgroundBox.tsx
@@ -0,0 +1,9 @@
+import Navbar from "./navbar";
+
+export default function BackgroundBox() {
+    return (
+        <div className="background-box">
+            <Navbar />
+        </div>
+    )
+}
---
+++ src/components/navbar.tsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+    return (
+        <div className="navbar">
+            <div className="logo">
+                <span className="dot"></span>
+                <div className="title">ジュース</div>
+            </div>
+            <span className="material-symbols-outlined shopping-icon">
+                local_mall
+            </span>
+        </div>
+    )
+}
